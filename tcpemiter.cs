c63cd63940a03840156df6b559e8ffcad7524eda
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-04-08 12:26:41 +0100

    Add files via upload

---
+++ tcpemiter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Usage: sendstring <ip address> <port> <string>");
+            return;
+        }
+
+        string ipAddress = args[0];
+        int port = int.Parse(args[1]);
+        string message = args[2];
+
+        try
+        {
+            TcpClient client = new TcpClient(ipAddress, port);
+            NetworkStream stream = client.GetStream();
+
+            byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+
+            Console.WriteLine("Sent: {0}", message);
+
+            stream.Close();
+            client.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: {0}", ex.Message);
+        }
+    }
+}
